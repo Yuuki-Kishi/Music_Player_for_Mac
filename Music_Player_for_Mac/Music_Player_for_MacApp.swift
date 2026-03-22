9850d6f8d22d542e42a18080499e83ef1a2ca25b
Yuuki-Kishi <yuuki.k.2006@gmail.com>
2026-03-23 02:30:56 +0900

    Initial Commit

---
+++ Music_Player_for_Mac/Music_Player_for_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_Player_for_MacApp.swift
+//  Music_Player_for_Mac
+//
+//  Created by 岸　優樹 on 2026/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_Player_for_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
